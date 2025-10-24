5487e7fe416b824effd1440fb9ad28a1cd126fae
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2025-10-24 17:22:07 -0400

    Bug arreglado foreign key sector for table company

--- database/migrations/2025_10_24_194450_add_sector_to_companies.php
+++ database/migrations/2025_10_24_194450_add_sector_to_companies.php
@@ -22,7 +22,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('companies', function (Blueprint $table) {
-            $table->dropColumn("sector_company_id");
+            $table->dropForeign(['sector_id']);
+            $table->dropColumn("sector_id");
         });
     }
 };
