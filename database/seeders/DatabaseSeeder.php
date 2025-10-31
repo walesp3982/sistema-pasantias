26a5be035dd23034ab06d9f2090be8c7d4ad9ac6
Walter Espejo <79987332+walesp3982@users.noreply.github.com>
2025-10-31 12:18:25 -0400

    Revert "Fix/seeder doc postulations"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,15 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Database\Seeders\Core\CareersSeeder;
-use Database\Seeders\Core\ChannelsSeeder;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\Core\PermissionSeeder;
-use Database\Seeders\Core\ShiftsSeeder;
-use Database\Seeders\Core\TypeDocumentsSeeder;
-use Database\Seeders\Core\TypeReportsSeeder;
-use Database\Seeders\Core\TypeSectorsSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,16 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-        $this->call([
-            // Core seeder
-            WorldSeeder::class,
-            CareersSeeder::class,
-            TypeReportsSeeder::class,
-            ChannelsSeeder::class,
-            ShiftsSeeder::class,
-            TypeSectorsSeeder::class,
-            PermissionSeeder::class,
-            TypeDocumentsSeeder::class
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
     }
 }
