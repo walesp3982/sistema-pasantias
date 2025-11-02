088f699e527f416723776fae4b59d60a07c85ad3
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2025-11-02 08:16:00 -0400

    Working in the login user

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -60,5 +63,16 @@ class User extends Authenticatable
             ->take(2)
             ->map(fn ($word) => Str::substr($word, 0, 1))
             ->implode('');
+    }
+
+    public function setActive(bool $value):bool {
+        return $this->update([
+            'active' => $value
+        ]);
+    }
+
+    #[Scope]
+    protected function active(Builder $query): void {
+        $query->where('active', true);
     }
 }
